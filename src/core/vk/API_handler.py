22300020d8f86b5c85284115db6bd4d30a5828ff
Lainina <ledenes04@mail.ru>
2023-08-24 19:11:30 +0300

    feat: added API polling structure, new schedule

--- src/core/vk/API_handler.py
+++ src/core/vk/API_handler.py
@@ -1,5 +1,6 @@
 import requests
 from src.core.vk.token import VK_TOKEN
+# TODO: add logging, replace all prints with logs
 
 
 class VkApiHandler:
@@ -7,6 +8,7 @@ class VkApiHandler:
         self.token = token
         self.version = '5.131'
         self.api_url = 'https://api.vk.com/method/'
+        self.group_id = '219138476'
 
     def send_message(self, text: str, peer_id=2000000002) -> int:
         params = {'peer_ids': str(peer_id),
@@ -38,6 +40,71 @@ class VkApiHandler:
             print('error: could not delete message')
             return
         return
+
+    def get_longpoll_server(self) -> dict[str, str]:
+        params = {'group_id': self.group_id,
+                  'access_token': self.token,
+                  'v': self.version}
+        url = self.api_url + 'groups.getLongPollServer'
+        response = requests.get(url=url, params=params)
+
+        if response.json().get('error', {}):
+            print('error: could not get long poll server')
+            return {}
+
+        return response.json().get('response', {})
+
+    def poll(self, key: str, server_url: str, ts: str) -> dict:
+        params = {'act': 'a_check',
+                  'key': key,
+                  'ts': ts,
+                  'wait': 25}
+        response = requests.get(url=server_url, params=params)
+
+        if response.json().get('failed', {}):
+            print('warning: failed to get updates, trying again...')
+            server = self.get_longpoll_server()
+
+            if not server:
+                print('error: could not restart server, terminating...')
+                return {}
+
+            params.update(server)
+            server_url = params.pop('server')
+            response = requests.get(url=server_url, params=params)
+
+            if response.json().get('failed', {}):
+                print('error: failed to get updates twice, terminating...')
+                return {}
+            print('warning: connection restored')
+
+        # TODO: format the info from response and return it
+        return {'ts': '7566', 'messages': [{'peer_id': 200000002, 'message_id': 17800, 'text': 'Это тест'}]}
+
+    def start_polling(self):
+        server = self.get_longpoll_server()
+
+        if not server:
+            print('error: failed to start polling')
+            return
+
+        key = server.get('response', {}).get('key', {})
+        server_url = server.get('response', {}).get('server', {})
+        ts = server.get('response', {}).get('ts', {})
+
+        while True:
+            updates = self.poll(key, server_url, ts)
+
+            if not updates:
+                print('terminal error: longpoll connection terminated')
+                return
+
+            ts = updates['ts']
+            messages = updates['messages']
+
+            if messages:
+                for message in messages:
+                    pass    # TODO: handle messages somehow
 
 
 vk = VkApiHandler(token=VK_TOKEN)
--- src/database/database.py
+++ src/database/database.py
@@ -1,5 +1,9 @@
 import json
 from datetime import datetime
+import locale
+
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 
 class Timetable:
@@ -18,26 +22,32 @@ class Timetable:
 
     def get_classes(self,
                     day: str,
-                    week_type: str) -> list[str, dict[str, str | int]]:
+                    week_type: str) -> dict[str, str]:
 
         day = day.capitalize()
 
         return self.timetable.get(week_type, {}).get(day, {})
 
-    def get_classes_for_today(self) -> list[str, dict[str, str | int]]:
+    def get_classes_for_today(self) -> dict[str, str]:
         today = datetime.today().strftime('%A')
         week_number = datetime.today().isocalendar()[1]
+
         week_type = 'odd' if week_number % 2 == 1 else 'even'
+
         return self.get_classes(today, week_type)
 
     def add_class(self,
+                  week_type: str,
                   day: str,
-                  time: str,
+                  number: int,
+                  start_time: str,
+                  end_time: str,
                   class_name: str,
-                  room_number: int,
-                  week_type: str) -> None:
+                  room_number: str,
+                  prof_name: str) -> None:
 
         day = day.capitalize()
+        number = str(number)
 
         if week_type not in self.timetable:
             self.timetable[week_type] = {}
@@ -45,21 +55,27 @@ class Timetable:
         if day not in self.timetable[week_type]:
             self.timetable[week_type][day] = {}
 
-        self.timetable[week_type][day][time] = {'class_name': class_name, 'room_number': room_number}
+        if number not in self.timetable[week_type][day]:
+            self.timetable[week_type][day][number] = {}
+
+        self.timetable[week_type][day][number] = {'start_time': start_time, 'end_time': end_time,
+                                                  'class_name': class_name,
+                                                  'room_number': room_number, 'prof_name': prof_name}
         self.save_schedule()
 
     def remove_class(self,
+                     week_type: str,
                      day: str,
-                     time: str,
-                     week_type: str) -> None:
+                     number: int) -> None:
 
         day = day.capitalize()
+        number = str(number)
 
         if week_type in self.timetable and day in self.timetable[week_type]:
 
-            if time in self.timetable[week_type][day]:
-                del self.timetable[week_type][day][time]
+            if number in self.timetable[week_type][day]:
+                del self.timetable[week_type][day][number]
                 self.save_schedule()
 
 
-timetable = Timetable("timetable.json")
+timetable = Timetable('timetable.json')
